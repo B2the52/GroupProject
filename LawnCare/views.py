d835cfc2b2f4750aeaad3ae48a29257cde80d65c
B2the52 <thies811@gmail.com>
2023-10-24 13:59:52 -0500

    added more

--- LawnCare/views.py
+++ LawnCare/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Service, Review, RequestContact, UserReview, Invoice
 from django.views import generic
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib import messages
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -34,3 +41,13 @@ def blog(request):
         'num_services': num_services
     }
     return render(request, 'blog.html', context=context)
+
+
+class ServiceCreate(CreateView):
+    model = Service
+    fields = ['service_id', 'service_title', 'service_info', 'service_cost']
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.save()
+        return HttpResponseRedirect(reverse('author_list'))
